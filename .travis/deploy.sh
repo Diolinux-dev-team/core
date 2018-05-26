e605bd7ecd00bdfc12720dde5983672edf0e0971
Gerardo Junior <gerardo.barbosa.da.silva.jr@gmail.com>
2018-05-26 14:09:08 -0300

    Travis auto deploy

---
+++ .travis/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DEPLOY_HOST=$([ "$TRAVIS_BRANCH" = "master" ] && echo $PRODUCTION_DEPLOY_HOST || echo $STAGING_DEPLOY_HOST)
+DEPLOY_PORT=$([ "$TRAVIS_BRANCH" = "master" ] && echo $PRODUCTION_DEPLOY_PORT || echo $STAGING_DEPLOY_PORT)
+DEPLOY_USER=$([ "$TRAVIS_BRANCH" = "master" ] && echo $PRODUCTION_DEPLOY_USER || echo $STAGING_DEPLOY_USER)
+DEPLOY_REPO=$([ "$TRAVIS_BRANCH" = "master" ] && echo $PRODUCTION_DEPLOY_REPO || echo $STAGING_DEPLOY_REPO)
+
+git config --global push.default matching
+git remote add deploy ssh://$DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PORT/$DEPLOY_REPO.git
+git remote add deploy ssh://$DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PORT/$DEPLOY_REPO.git
+git add -f vendor
+git commit --author="Auto deploy by travis-ci <build@travis-ci.org>" -m "Auto deploy: $(date +'%Y-%m-%d %T')"
+git push deploy master
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+#ssh $DEPLOY_USER@$DEPLOY_HOST -p $DEPLOY_PORT <<EOF
+#  cd $DEPLOY_DIR # Change to whatever commands you need!
+#EOF
