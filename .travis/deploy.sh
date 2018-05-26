86ac7922da6dad8ecdbcc70c0ab11f3efa924c28
Gerardo Junior <gerardo.barbosa.da.silva.jr@gmail.com>
2018-05-26 14:37:31 -0300

    Change from sh to bash

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 DEPLOY_HOST=$([ "$TRAVIS_BRANCH" = "master" ] && echo $PRODUCTION_DEPLOY_HOST || echo $STAGING_DEPLOY_HOST)
 DEPLOY_PORT=$([ "$TRAVIS_BRANCH" = "master" ] && echo $PRODUCTION_DEPLOY_PORT || echo $STAGING_DEPLOY_PORT)
@@ -6,7 +6,6 @@ DEPLOY_USER=$([ "$TRAVIS_BRANCH" = "master" ] && echo $PRODUCTION_DEPLOY_USER ||
 DEPLOY_REPO=$([ "$TRAVIS_BRANCH" = "master" ] && echo $PRODUCTION_DEPLOY_REPO || echo $STAGING_DEPLOY_REPO)
 
 git config --global push.default matching
-git remote add deploy ssh://$DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PORT/$DEPLOY_REPO.git
 git remote add deploy ssh://$DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PORT/$DEPLOY_REPO.git
 git add -f vendor
 git commit --author="Auto deploy by travis-ci <build@travis-ci.org>" -m "Auto deploy: $(date +'%Y-%m-%d %T')"
